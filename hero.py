d35f53b51fed4a5ba2115d2bd685018b3351b04b
maxcrimea <maxcrimea50@gmail.com>
2023-01-08 00:13:05 +0600

    'hw2'

--- hero.py
+++ hero.py
@@ -21,9 +21,78 @@ class SuperHero:
         return len(self.catchphrase)
 
 
-Hero = SuperHero('Flasher', 'Flasher', 'Freeze', 100, 'I am Flash') # передал значения в объект класса для проверки работы кода
+Hero = SuperHero('Flasher', 'Flasher', 'Freeze', 100,
+                 'I am Flash')  # передал значения в объект класса для проверки работы кода
 
 Hero.show_name()
 print(Hero.mul())
 print(Hero)
 print(len(Hero))
+
+
+class SuperHero2(SuperHero):
+    people1 = 'people1'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = False
+        self.fly = False
+
+    def mul(self):
+        super().mul()
+        self.fly = True
+        return self.health_points ** 2
+
+    def view_fly(self):
+        print('fly in the True_phrase')
+
+    def __str__(self):
+        return f'{self.fly} {self.superpower} {self.health_points}'
+
+
+Hero2 = SuperHero2('Flasher', 'Flasher', 'Freeze', 100, 'I am Flash', 50, 'fly')
+print(Hero2.mul())
+Hero2.view_fly()
+print(Hero2)
+
+
+class SuperHero3(SuperHero):
+    people2 = 'people2'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = False
+        self.fly = False
+
+    def mul(self):
+        super().mul()
+        self.fly = True
+        return self.health_points ** 2
+
+    def view_fly(self):
+        print('fly in the True_phrase')
+
+
+Hero3 = SuperHero3('Flasher', 'Flasher', 'Freeze', 80, 'I am Flash', 70, 'fly')
+print(Hero3.mul())
+Hero3.view_fly()
+print(Hero3)
+
+
+class villain(SuperHero3):
+    people = 'monster'
+
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly):
+        super().__init__(name, nickname, superpower, health_points, catchphrase, damage, fly)
+
+    def gen_x(self):
+        return None
+
+    def crit(self):
+        return self.damage ** 2
+
+
+vill = villain('Flasher', 'Flasher', 'Freeze', 60, 'I am Flash', 70, 'fly')
+vill.gen_x()
+print(villain.crit(Hero2))
+print(vill.gen_x())
